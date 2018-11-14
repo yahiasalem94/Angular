96de5c90d932464507d571824d5b85db03cf87aa
Yahia Salem <yahiah.salem@gmail.com>
2018-11-14 15:28:23 +0100

    Base Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { DishService } from './services/dish.service';
 
 import 'hammerjs';
 
@@ -30,7 +31,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
