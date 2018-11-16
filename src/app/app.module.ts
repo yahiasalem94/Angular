600552ed1fe19d850f9071c630f34ad92ca9804e
Yahia Salem <yahiah.salem@gmail.com>
2018-11-16 11:56:38 +0100

    Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LeaderService } from './services/leader.service';
 
 @NgModule({
   declarations: [
@@ -46,7 +47,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   providers: [
   DishService, 
-  PromotionService
+  PromotionService, 
+  LeaderService 
   ],
   bootstrap: [AppComponent]
 })
